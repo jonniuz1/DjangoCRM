dde5cde7acc4bc24e033aaf89242177be85aa972
jonniuz1 <101719360+jonniuz1@users.noreply.github.com>
2023-12-27 20:01:16 +0900

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=False)
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.onrender.com']
 
 # Application definition
 
